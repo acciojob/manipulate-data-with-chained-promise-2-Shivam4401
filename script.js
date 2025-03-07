58548b8234a4717d5b7cc164340679ffdd206567
Shivam kumar <146973981+Shivam4401@users.noreply.github.com>
2025-03-07 13:02:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+// initial promise
+let initialPromise = new Promise((resolve,reject) => {
+	  setTimeout(() => {
+		  resolve([1,2,3,4]);
+	  },3000);
+});
+
+initialPromise.then((number) => {
+	let evenNumber = number.filter((num) => num%2 == 0);
+	document.querySelector("#input").textContent = evenNumber;
+	return new Promise((resolve,reject) => {
+       setTimeout(() => {
+		   resolve(evenNumber);
+	   },1000);
+	});
+})
+.then((evenNumber) => {
+	let multiplyBy = evenNumber.map((num) => num*2);
+	document.querySelector("#input").textContent = multiplyBy;
+	return new Promise ((resolve,reject) => {
+		setTimeout(() => {
+			resolve(multiplyBy);
+		},2000);
+	});
+
+	
+})
